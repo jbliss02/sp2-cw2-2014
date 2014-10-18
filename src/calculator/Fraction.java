d63b0f9eab44744061ac9318105710f53e43d2c6
jbliss02 <jbliss02@dcs.bbk.ac.uk>
2014-10-18 21:17:32 +0100

    .

---
+++ src/calculator/Fraction.java
@@ -0,0 +1,10 @@
+package calculator;
+
+public class Fraction {
+
+	public Fraction()
+	{
+		
+	}
+	
+}
---
+++ src/calculator/FractionTest.java
@@ -0,0 +1,14 @@
+package calculator;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FractionTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
