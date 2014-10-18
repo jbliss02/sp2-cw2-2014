c1bb01a8f5df3e5369e1a1755f894a45e5c79269
jbliss02 <jbliss02@dcs.bbk.ac.uk>
2014-10-18 21:19:12 +0100

    base

--- src/calculator/FractionTest.java
+++ src/calculator/FractionTest.java
@@ -1,14 +1,35 @@
+/**
+ * 
+ */
 package calculator;
 
 import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+/**
+ * @author J
+ *
+ */
 public class FractionTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testMultiply() {
+		
+		assertEquals("Wrong", new Fraction(3, 10), new Fraction(1, 2).multiply(new Fraction(3, 5)));
+		assertEquals("Wrong", new Fraction(2, 35), new Fraction(4, 7).multiply(new Fraction(1, 10)));
+	}
+	
+	@Test
+	public void TestEquals()
+	{
+	    assertEquals ("Wrong", new Fraction(1, 2), new Fraction(1, 2));
+	    assertEquals ("Wrong", new Fraction(1, 2), new Fraction(3, 6));
+	    assertEquals ("Wrong", new Fraction(1, 2), new Fraction(3, 6));    
+	    //assertEquals ("Wrong", new Fraction(1, 2), new Fraction(1, -2));	    
+	    //assertEquals ("Wrong", new Fraction(-1, 2), new Fraction(1, -2));	
+	   // assertEquals ("Wrong", new Fraction(-1, -2), new Fraction(1, -2));
+	    //
 	}
 
 }
