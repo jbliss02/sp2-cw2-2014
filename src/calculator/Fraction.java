7454587d4bc527c72f2b64370bb211ba1b9ffb11
jbliss02 <jbliss02@dcs.bbk.ac.uk>
2014-10-19 13:12:22 +0100

    start of FractionCalculator written isNumeric and isFraction

--- src/calculator/Fraction.java
+++ src/calculator/Fraction.java
@@ -94,11 +94,7 @@ public class Fraction {
     public Fraction negate()
     {//negates the objects fraction, if either the numerator or denominator are negative then change the sign so
      //both are always positive
-    	
-//    	int newNom = this.getNumerator() < 0 ? this.getNumerator() * -1 : this.getNumerator();
-//    	int newDenom = this.getDenominator() < 0 ? this.getDenominator() * -1 : this.getDenominator();
-//    	return new Fraction(newNom, newDenom).simplify();
-    	
+
     	return new Fraction(this.getNumerator(), this.getDenominator() * -1);
     	
     }
